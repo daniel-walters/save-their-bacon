07481a7799940cc3f06cc4b63f12e9fa6197bf9d
daniel-walters <waltersdaniel96@gmail.com>
2021-07-26 11:40:07 +0800

    adds views/actions for home and account page. Changes landing page depending on if user is already signed in or not

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+  #send user to their home page if they are already signed in - otherwise go to index
+  before_action :redirect_signed_in_user, only: [:index]
+
   def index
+  end
+
+  def home
+  end
+
+  def account
+  end
+
+  private
+
+  def redirect_signed_in_user
+    redirect_to user_home_path if user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#index"
+
+  get "/home", to: "pages#home", as: "user_home"
+  get "/account", to: "pages#account", as: "user_account"
 end
