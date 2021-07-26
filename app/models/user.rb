3d73ec510fd13f7954a88f34bf9b62a48167485e
daniel-walters <waltersdaniel96@gmail.com>
2021-07-26 13:57:13 +0800

    passes role to sign up page and sets approved field depending on role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_create :set_approval
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,4 +11,16 @@ class User < ApplicationRecord
     sanctuary: 1,
     admin: 2
   }
+
+  private
+
+  #set all sanctuary account to unaproved on creation
+  def set_approval
+    self.sanctuary? ? self.approved = false : self.approved = true
+    #if !self.sanctuary?
+      #puts "not a sanctuary"
+      #self.approved = true
+      #puts "approved: #{self.approved?}"
+    #end
+  end
 end
