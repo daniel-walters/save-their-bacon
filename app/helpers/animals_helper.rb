b9e55c566c89ab1f650a5867a033e6e2b032b42c
daniel-walters <waltersdaniel96@gmail.com>
2021-07-28 09:52:42 +0800

    adds default image to animals

--- app/helpers/animals_helper.rb
+++ app/helpers/animals_helper.rb
@@ -1,2 +1,7 @@
 module AnimalsHelper
+    def image_select(animal)
+        return animal.profile_picture if animal.profile_picture.attached?
+        #use placeholder if no img attached
+        return "animal_placeholder.png"
+    end
 end
